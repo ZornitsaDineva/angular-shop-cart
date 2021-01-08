14dab327c33549a817e2c66558cf4a1ce54cbe53
ZornitsaDineva <zornitsa_angelova@yahoo.com>
2021-01-08 14:02:01 +0200

    2 files modified

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,6 @@ export class ServiceCartService {
     return this.items;
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
 }
