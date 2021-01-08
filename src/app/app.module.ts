681e47eefaba383f22f9085317613bc7bc1de800
ZornitsaDineva <zornitsa_angelova@yahoo.com>
2021-01-08 13:01:15 +0200

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ServiceCartService } from './service.service';
 
 @NgModule({
   imports: [
@@ -27,7 +28,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   ],
   bootstrap: [
     AppComponent
-  ]
+  ],
+  providers: [ServiceCartService]
 })
 export class AppModule { }
 
